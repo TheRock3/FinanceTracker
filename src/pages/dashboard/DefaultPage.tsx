7a12e0d141c20069a1e094065ce3fa9085560505
TheRock3 <35250090+TheRock3@users.noreply.github.com>
2025-03-24 00:10:02 +0530

    added pagination

--- src/pages/dashboard/DefaultPage.tsx
+++ src/pages/dashboard/DefaultPage.tsx
@@ -1,11 +1,62 @@
-import React from 'react';
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import PaginationRounded from '../../components/common/Pagination';
 
-type Props = {};
+function createData(
+  name: string,
+  calories: number,
+  fat: number,
+  carbs: number,
+  protein: number,
+) {
+  return { name, calories, fat, carbs, protein };
+}
 
-const DefaultPage = (props: Props) => {
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
+export default function DefaultPage() {
   return (
-    <div>DefaultPage</div>
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Stock Name</TableCell>
+            <TableCell align="right">Purchased Price</TableCell>
+            <TableCell align="right">Current Price</TableCell>
+            <TableCell align="right">Profit/Loss (Amount)</TableCell>
+            <TableCell align="right">Profit/Loss (%)</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.calories}</TableCell>
+              <TableCell align="right">{row.fat}</TableCell>
+              <TableCell align="right">{row.carbs}</TableCell>
+              <TableCell align="right">{row.protein}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      <PaginationRounded/>
+    </TableContainer>
   );
-};
-
-export default DefaultPage;
+}

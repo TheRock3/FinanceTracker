fb61039b1d773ca544ddeb86f67e59971fd8f3b3
TheRock3 <35250090+TheRock3@users.noreply.github.com>
2025-05-01 12:38:05 +0530

    use Absolute imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { MainLayout } from 'components/layout/MainLayout';
 import { routes } from "./routes";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/">
+        <Route path="/" element={<MainLayout />}>
           {routes}
         </Route>
       </Routes>
